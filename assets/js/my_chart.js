fdd9605e2b798bb725c81fd9076327e98ccf3fde
yayzee0 <111242785+nerd-people@users.noreply.github.com>
2024-10-24 16:24:18 +0700

    Add files via upload

---
+++ assets/js/my_chart.js
@@ -0,0 +1,49 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Tokopedia', 'Shopee', 'Tiktok Shop'],
+        datasets: [{
+            label: 'Traffic Source',
+            data: [85, 63, 70],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [54, 69, 78, 39, 62, 35, 79, 55, 78, 65, 83, 90],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
